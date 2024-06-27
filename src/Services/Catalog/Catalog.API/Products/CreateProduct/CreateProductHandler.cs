11777cb5e21c1a857cf69117547a53d834b26181
RYE <robbyeadam@gmail.com>
2024-06-27 21:48:36 +0900

    added Catalog API

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name, string Description, List<string> Categories, string ImageFile, decimal Price)
+        : ICommand<CreateProductResponse>;
+
+
+    public record CreateProductResponse(
+        Guid Id);
+
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
+    {
+        public async Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = command.Name,
+                Description = command.Description,
+                Categories = command.Categories,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            return new CreateProductResponse(product.Id);
+            
+        }
+    }
+}
